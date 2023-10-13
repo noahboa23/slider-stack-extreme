b20818b099e88a5b7a1fc34e502deb23626f9c84
Cicerokx7 <104519279+Cicerokx7@users.noreply.github.com>
2023-10-13 03:53:04 -0500

    Spawning
    Items can now spawn from the sky at random locations in front of the player.

---
+++ Unity Project/Assets/spawnFood.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnFood : MonoBehaviour
+{
+    public GameObject[] food;
+    public Transform orientation;
+    private float lastTime;
+    void Start()
+    {
+        lastTime = Time.fixedTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //If at least one second has passed spawn food.
+        if((Time.fixedTime-lastTime) >= 2){
+            lastTime = Time.fixedTime;
+            //Chose food item at random.
+            int randomIndex = Random.Range(0, food.Length);
+            //Choose a random spawn spot based on the spawn sphere.
+            Vector3 randomPosition = new Vector3(Random.Range(-10+orientation.position.x, 11+orientation.position.x), 15, Random.Range(-10+orientation.position.z,10+orientation.position.z));
+            //Spawn the food item.
+            Instantiate(food[randomIndex], randomPosition, Quaternion.identity);
+        }
+    }
+}
